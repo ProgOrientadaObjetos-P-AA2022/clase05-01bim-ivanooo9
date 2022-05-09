dcb62c034e9fb79ec6ba7e04e89c7f2e79dd7617
ivanooo9 <marcoeli098@gmail.com>
2022-05-09 14:23:22 -0500

    clase 5

--- EjemplosRelacionesClases/EjemploRelaciones/src/paquete01/UnidadDVD.java
+++ EjemplosRelacionesClases/EjemploRelaciones/src/paquete01/UnidadDVD.java
@@ -3,14 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paqueteuno;
+package paquete01;
 
 public class UnidadDVD {
 
     private String marca;
-
+    private double costo;
+    
     public UnidadDVD(String a) {
         marca = a;
+    }
+    public UnidadDVD(String a, double c) {
+        marca = a;
+        costo = c;
     }
 
     public void establecerMarca(String n) {
